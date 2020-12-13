3267ad4ca908bac8a5181b7e47ee75155356ebca
JUAN MANUEL PATIÑO VALENCIA <juan.patino.662@unisabaneta.edu.co>
2020-12-13 17:28:53 +0000

    Estructuras de rutas listas

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+#Imports
 from flask import	Flask, jsonify
+from data import products
 
+#Instancia
 app = Flask(__name__)
 
+#Rutas
 @app.route('/', methods = ['GET'])
 def index():
-    return jsonify({"msssg": "hola"})
+    return jsonify({"Rutas":
+                   {"/":"Bienvenida y Rutas [GET]", 
+                    "/data": "informacion a cerca de los integrantes de la banda [GET]",
+                    "/data/{id}":"Informacion a cerca de un integrante de la banda [GET] [{id}]",
+                     "/create": "crea un integrante de la banda [POST]",
+                      "/modify/{id}": "modifica un integrante de la banda [patch]"}})
 
 @app.route('/data', methods = ['GET'])
-def info():
+def all_data():
     return jsonify({"data": "Information"})
 
+@app.route('/data/{id}', methods = ['GET'])
+def data_by_id():
+    return jsonify({"data": "Information"})
 
+@app.route('/create', methods = ['POST'])
+def create_data():
+    return jsonify({"data": "Information"})
 
+@app.route('/modify/{id}', methods = ['patch'])
+def update_data():
+    return jsonify({"data": "Information"})
+
+#Punto de entrada
 if __name__ == "__main__":
-    
     app.run(debug=True, host='0.0.0.0' , port=4000)
