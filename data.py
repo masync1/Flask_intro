c3a14125d4cc2cabde13bec55abcd10a3b0b84cf
JUAN MANUEL PATIÑO VALENCIA <juan.patino.662@unisabaneta.edu.co>
2020-12-13 17:12:40 +0000

    Create data.py

---
+++ data.py
@@ -0,0 +1,22 @@
+
+products = [
+    
+    {"name":"Julian", 
+     "last_name": "Soto",
+     "email":"jdsoto@gmail.com"},  
+    {"name":"julian ",
+    "last_name": "correa",
+     "email":"jcorrea@gmail.com"},
+    {"name":"Oliver ", 
+    "last_name": "charry",
+     "email":"ofcharry@gmail.com"},  
+    {"name":"Mateo ",
+    "last_name": "Garcia",
+     "email":"mgarcia@gmail.com"},
+    {"name":"Emilio ", 
+    "last_name": "Peña",
+     "email":"ejpena@gmail.com"},  
+    {"name":"Juan ",
+    "last_name": "Patiño",
+     "email":"jmpatino@gmail.com"}  
+    ]
