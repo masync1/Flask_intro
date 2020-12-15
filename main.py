9a1d1d49602ca9e32970565d8d99d0682a84a9e4
JUAN MANUEL PATIÑO VALENCIA <juan.patino.662@unisabaneta.edu.co>
2020-12-15 01:29:33 +0000

    Format to file

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 #Imports
-from flask import	Flask, jsonify
+from flask import Flask, jsonify
 from data import products, Rutas
 
 #Instancia
 app = Flask(__name__)
 
 
-
 #Rutas
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def index():
-  response = jsonify({"information": Rutas})
-  return response
+    response = jsonify({"information": Rutas})
+    return response
 
-@app.route('/data', methods = ['GET'])
+
+@app.route('/data', methods=['GET'])
 def all_data():
     return jsonify({"data": products})
 
-@app.route('/data/{id}', methods = ['GET'])
+
+@app.route('/data/{id}', methods=['GET'])
 def data_by_id():
     return jsonify({"data": "Information"})
 
-@app.route('/create', methods = ['POST'])
+
+@app.route('/create', methods=['POST'])
 def create_data():
     return jsonify({"data": "Information"})
 
-@app.route('/modify/{id}', methods = ['patch'])
+
+@app.route('/modify/{id}', methods=['patch'])
 def update_data():
     return jsonify({"data": "Information"})
 
+
 #Punto de entrada
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0' , port=4000)
+    app.run(debug=True, host='0.0.0.0', port=4000)
